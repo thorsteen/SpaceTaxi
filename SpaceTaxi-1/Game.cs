2beee8d75467ed86bb0d8975c9289369e79039d1
Thor <tsl19@hotmail.com>
2019-04-16 11:50:54 +0200

    space taxi 1 cs proj + files

---
+++ SpaceTaxi-1/Game.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SpaceTaxi_1 {
+    public class Game : IGameEventProcessor<object> {
+        private Entity backGroundImage;
+        private GameEventBus<object> eventBus;
+        private GameTimer gameTimer;
+        private Player player;
+        private Window win;
+
+        public Game() {
+            // window
+            win = new Window("Space Taxi Game v0.1", 500, AspectRatio.R1X1);
+
+            // event bus
+            eventBus = new GameEventBus<object>();
+            eventBus.InitializeEventBus(new List<GameEventType> {
+                GameEventType.InputEvent, // key press / key release
+                GameEventType.WindowEvent, // messages to the window, e.g. CloseWindow()
+                GameEventType.PlayerEvent // commands issued to the player object, e.g. move,
+                                          // destroy, receive health, etc.
+            });
+            win.RegisterEventBus(eventBus);
+
+            // game timer
+            gameTimer = new GameTimer(60); // 60 UPS, no FPS limit
+
+            // game assets
+            backGroundImage = new Entity(
+                new StationaryShape(new Vec2F(0.0f, 0.0f), new Vec2F(1.0f, 1.0f)),
+                new Image(Path.Combine("Assets", "Images", "SpaceBackground.png"))
+            );
+            backGroundImage.RenderEntity();
+
+            // game entities
+            player = new Player();
+            player.SetPosition(0.45f, 0.6f);
+            player.SetExtent(0.1f, 0.1f);
+
+            // event delegation
+            eventBus.Subscribe(GameEventType.InputEvent, this);
+            eventBus.Subscribe(GameEventType.WindowEvent, this);
+            eventBus.Subscribe(GameEventType.PlayerEvent, player);
+        }
+
+        public void GameLoop() {
+            while (win.IsRunning()) {
+                gameTimer.MeasureTime();
+
+                while (gameTimer.ShouldUpdate()) {
+                    win.PollEvents();
+                    eventBus.ProcessEvents();
+                }
+
+                if (gameTimer.ShouldRender()) {
+                    win.Clear();
+                    backGroundImage.RenderEntity();
+                    player.RenderPlayer();
+
+                    win.SwapBuffers();
+                }
+
+                if (gameTimer.ShouldReset()) {
+                    // 1 second has passed - display last captured ups and fps from the timer
+                    win.Title = "Space Taxi | UPS: " + gameTimer.CapturedUpdates + ", FPS: " +
+                                 gameTimer.CapturedFrames;
+                }
+            }
+        }
+
+        public void KeyPress(string key) {
+            switch (key) {
+            case "KEY_ESCAPE":
+                win.CloseWindow();
+                break;
+            case "KEY_F12":
+                Console.WriteLine("Saving screenshot");
+                win.SaveScreenShot();
+                break;
+            case "KEY_UP":
+                eventBus.RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.PlayerEvent, this, "BOOSTER_UPWARDS", "", ""));
+                break;
+            case "KEY_LEFT":
+                eventBus.RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.PlayerEvent, this, "BOOSTER_TO_LEFT", "", ""));
+                break;
+            case "KEY_RIGHT":
+                eventBus.RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.PlayerEvent, this, "BOOSTER_TO_RIGHT", "", ""));
+                break;
+            }
+        }
+
+        public void KeyRelease(string key) {
+            switch (key) {
+            case "KEY_LEFT":
+                eventBus.RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.PlayerEvent, this, "STOP_ACCELERATE_LEFT", "", ""));
+                break;
+            case "KEY_RIGHT":
+                eventBus.RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.PlayerEvent, this, "STOP_ACCELERATE_RIGHT", "", ""));
+                break;
+            case "KEY_UP":
+                eventBus.RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.PlayerEvent, this, "STOP_ACCELERATE_UP", "", ""));
+                break;
+            }
+        }
+
+        public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
+            if (eventType == GameEventType.WindowEvent) {
+                switch (gameEvent.Message) {
+                case "CLOSE_WINDOW":
+                    win.CloseWindow();
+                    break;
+                }
+            } else if (eventType == GameEventType.InputEvent) {
+                switch (gameEvent.Parameter1) {
+                case "KEY_PRESS":
+                    KeyPress(gameEvent.Message);
+                    break;
+                case "KEY_RELEASE":
+                    KeyRelease(gameEvent.Message);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ SpaceTaxi-1/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace SpaceTaxi_1 {
+    internal class Program {
+        public static void Main(string[] args) {
+            var game = new Game();
+            game.GameLoop();
+        }
+    }
+}
