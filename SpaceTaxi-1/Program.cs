037e1ed8bd0d4b6c18f1f671dd7736aeab588a3b
Thor <tsl19@hotmail.com>
2019-04-26 09:40:31 +0200

    File stream reader class skabelon

--- SpaceTaxi-1/Program.cs
+++ SpaceTaxi-1/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace SpaceTaxi_1 {
     internal class Program {
         public static void Main(string[] args) {
+            Test.Method(); /// test af file stream reader class skabelon
             var game = new Game();
             game.GameLoop();
         }
