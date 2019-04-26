7d32f12ff59324a4342bdbec4165e875acda1481
Bertram P <BassiBU@gmail.com>
2019-04-26 14:07:13 +0200

    Rettelse - Did nothing :(

--- SpaceTaxi-1/LevelRender.cs
+++ SpaceTaxi-1/LevelRender.cs
@@ -17,14 +17,17 @@ namespace SpaceTaxi_1 {
             List<Entity> listEntity = new List<Entity>();
             for (int i = 0; i < 23; i++) {
                 for (int j = 0; j < 40; j++) {
-                    char temp = level.map[i][j];
-                    string fileName = level.keyLegend[temp];
-                    Entity entity = new Entity(new StationaryShape(new Vec2F(1f / 40f * j, 1f / 
-                    23f * i), new 
-                    Vec2F(
-                        1f / 40f,
-                        1f / 23f)), new Image(Path.Combine("Assets", "Images", fileName)));
-                    listEntity.Add(entity);
+                    char symbol = level.map[i][j];
+                    if (symbol != ' ') {
+                        string fileName = level.keyLegend[symbol];
+                        Entity entity = new Entity(new StationaryShape(new Vec2F(1f / 40f * j, 1f / 
+                                                                                               23f * i), new 
+                            Vec2F(
+                                1f / 40f,
+                                1f / 23f)), new Image(Path.Combine("Assets", "Images", fileName)));
+                        listEntity.Add(entity);
+                    }
+                    
                 }
             }
 
