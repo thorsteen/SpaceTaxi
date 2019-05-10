4622e378710dffbca11b555f0e034afa4756c4eb
Theodor7 <theodor.b.hansen@gmail.com>
2019-05-10 13:22:47 +0200

    Level nu tilgængeligt fra menuen
    LEVEL VALG VIRKER ENDNU IKKE

--- SpaceTaxi-2/Game.cs
+++ SpaceTaxi-2/Game.cs
@@ -12,23 +12,20 @@ using SpaceTaxi_2.Taxi;
 
 namespace SpaceTaxi_2 {
     public class Game : IGameEventProcessor<object> {
-        private Entity backGroundImage;
+        
         public GameEventBus<object> eventBus;
         private GameTimer gameTimer;
-        private Player player;
+        
         private Window win;
-        private Level level;
-        private List<Entity> EList;
-        private LevelRender levelRender;
-        public LevelParser levelParser;
-        public Customer customer;
+        
+        
+        
+        
         public StateMachine stateMachine;
 
         public Game() {
             // window
             win = new Window("Space Taxi Game v0.1", 500, AspectRatio.R1X1);
-            levelParser = new LevelParser();
-            level = levelParser.CreateLevel("the-beach.txt");
 
             // event bus
             eventBus = EventBus.GetBus();
@@ -48,29 +45,20 @@ namespace SpaceTaxi_2 {
             gameTimer = new GameTimer(60); // 60 UPS, no FPS limit
 
             // game assets
-            backGroundImage = new Entity(
-                new StationaryShape(new Vec2F(0.0f, 0.0f), new Vec2F(1.0f, 1.0f)),
-                new Image(Path.Combine("Assets", "Images", "SpaceBackground.png"))
-            );
-            backGroundImage.RenderEntity();
+            
 
             // game entities
-            player = new Player();
-            player.SetPosition(0.45f, 0.6f);
-            player.SetExtent(0.08f, 0.08f);
+            
 
             // event delegation
             eventBus.Subscribe(GameEventType.InputEvent, this);
             eventBus.Subscribe(GameEventType.WindowEvent, this);
-            eventBus.Subscribe(GameEventType.PlayerEvent, player);
+            
             
             /// level rendering
-            levelRender = new LevelRender();
-            EList = levelRender.LevelToEntityList(level);
             
-            /// customer
-            customer = new Customer("Hello",new DynamicShape(new Vec2F(0.45f,0.1f),new Vec2F(0.1f,0.1f)),
-                new Image(Path.Combine("Assets","Images","CustomerStandLeft.png")));
+            
+            
             
             ///
             stateMachine = new StateMachine();
@@ -78,21 +66,15 @@ namespace SpaceTaxi_2 {
         }
         
         
-        public void SetLevel(string levelFileName) { //sets a level
-            level = levelParser.CreateLevel(levelFileName);
-            EList = levelRender.LevelToEntityList(level);
-            player.SetPosition(0.45f, 0.6f);
-            player.Velocity.X = 0.0f;
-            player.Velocity.Y = 0.004f;
-        }
+        
         
         public void GameLoop() {
             while (win.IsRunning()) {
                 gameTimer.MeasureTime();
 
                 while (gameTimer.ShouldUpdate()) {
+                    stateMachine.ActivateState.UpdateGameLogic();
                     win.PollEvents();
-                    player.UpdateTaxi();
                     eventBus.ProcessEvents();
                     
                 }
@@ -117,14 +99,6 @@ namespace SpaceTaxi_2 {
             case "KEY_ESCAPE":
                 win.CloseWindow();
                 break;
-            case "KEY_F11":
-                Console.WriteLine("Change Level to: ");
-                string newLevel = Console.ReadLine();
-                Console.WriteLine("Changing level to " + newLevel);
-                SetLevel(newLevel);
-                Console.WriteLine(level.mapName);
-                break;
-                
             case "KEY_F12":
                 Console.WriteLine("Saving screenshot");
                 win.SaveScreenShot();
--- SpaceTaxi-2/States/GameRunning.cs
+++ SpaceTaxi-2/States/GameRunning.cs
@@ -1,10 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using DIKUArcade.Entities;
 using DIKUArcade.EventBus;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 using DIKUArcade.State;
+using SpaceTaxi_2.Taxi;
 
 namespace SpaceTaxi_2.States {
     public class GameRunning : IGameState{
         
+        public GameEventBus<object> eventBus;
+        private List<Entity> EList;
+        private Entity backGroundImage;
+        
+        private Player player;
+        public Customer customer;
+        private Level level;
+        public LevelParser levelParser;
+        private LevelRender levelRender;
+        
         private static GameRunning instance = null;
+
+        public GameRunning() {
+            eventBus = EventBus.GetBus();
+            
+            levelParser = new LevelParser();
+            level = levelParser.CreateLevel("the-beach.txt");
+            
+            player = new Player();
+            player.SetPosition(0.45f, 0.6f);
+            player.SetExtent(0.08f, 0.08f);
+            
+            eventBus.Subscribe(GameEventType.PlayerEvent, player);
+            
+            levelRender = new LevelRender();
+            EList = levelRender.LevelToEntityList(level);
+            
+            backGroundImage = new Entity(
+                new StationaryShape(new Vec2F(0.0f, 0.0f), new Vec2F(1.0f, 1.0f)),
+                new Image(Path.Combine("Assets", "Images", "SpaceBackground.png"))
+            );
+            backGroundImage.RenderEntity();
+            
+            /// customer
+            customer = new Customer("Hello",new DynamicShape(new Vec2F(0.45f,0.1f),new Vec2F(0.1f,0.1f)),
+                new Image(Path.Combine("Assets","Images","CustomerStandLeft.png")));
+            
+        }
+
         
         public static GameRunning GetInstance() {
             return GameRunning.instance ?? (GameRunning.instance = new GameRunning());
@@ -17,15 +62,28 @@ namespace SpaceTaxi_2.States {
         }
 
         public void UpdateGameLogic() {
+            player.UpdateTaxi();
 
         }
 
         public void RenderState() {
+            foreach (var texture in EList) {
+                texture.RenderEntity();
+                player.RenderPlayer();
+            }
+        }
+        
+        public void SetLevel(string levelFileName) { //sets a level
+            level = levelParser.CreateLevel(levelFileName);
+            EList = levelRender.LevelToEntityList(level);
+            player.SetPosition(0.45f, 0.6f);
+            player.Velocity.X = 0.0f;
+            player.Velocity.Y = 0.004f;
         }
 
         public void HandleKeyEvent(string keyValue, string keyAction) {
           switch (keyValue) {
-            case "KEY_PRESS":
+            case "KEY_RELEASE":
                 switch (keyAction) {
                     
                     case "KEY_LEFT":
@@ -43,11 +101,14 @@ namespace SpaceTaxi_2.States {
                                             GameEventFactory<object>.CreateGameEventForAllProcessors(
                                                 GameEventType.PlayerEvent, this, "STOP_ACCELERATE_UP", "", ""));
                                         break;
+                    
+                
+                    
                 }
 
                 break;
 
-            case "KEY_RELEASE":
+            case "KEY_PRESS":
                 switch (keyAction) {
                                   case "KEY_UP":
                                       EventBus.GetBus().RegisterEvent(
@@ -64,6 +125,16 @@ namespace SpaceTaxi_2.States {
                                             GameEventFactory<object>.CreateGameEventForAllProcessors(
                                                 GameEventType.PlayerEvent, this, "BOOSTER_TO_RIGHT", "", ""));
                                         break;
+                                    
+                                  case "KEY_F1":
+                                      Console.WriteLine("Changing level to THE BEACH");
+                                      SetLevel("the-beach.txt");
+                                      break;
+                                  
+                                  case "KEY_F2":
+                                      Console.WriteLine("Changing level to THE BEACH");
+                                      SetLevel("short-n-sweet.txt");
+                                      break;
                 }
 
                 break;
