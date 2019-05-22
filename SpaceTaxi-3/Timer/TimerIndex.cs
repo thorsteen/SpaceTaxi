908a4caca57e8b45fc29b2777e0263d9733f0b08
Bertram P <BassiBU@gmail.com>
2019-05-22 14:28:06 +0200

    XML til TimeIndex

--- SpaceTaxi-3/Timer/TimerIndex.cs
+++ SpaceTaxi-3/Timer/TimerIndex.cs
@@ -5,7 +5,14 @@ namespace SpaceTaxi_3.Timer {
     public class TimerIndex {
         private int timer = 0;
         private int oneMin = 0;
-
+        
+        /// <summary>
+        /// A void that keeps track of time. Counts from 1-59 sec, and then adds one to the minute counter.
+        /// Though because of the values being remebered by the class, every new call should be to the time wanted,
+        /// and not the time you want to add.
+        /// </summary>
+        /// <param name="minutes"></param>
+        /// <param name="seconds"></param>
         public void PrintSeconds(int minutes, int seconds) {
             while (timer < seconds || oneMin < minutes) {
                 timer += 1;
