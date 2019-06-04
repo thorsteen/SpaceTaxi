99224db237a6fcebf6d5993c23829c2f97a99c5c
Theodor7 <theodor.b.hansen@gmail.com>
2019-06-04 12:55:47 +0200

    Level refactoring

--- SpaceTaxi-3/Level.cs
+++ SpaceTaxi-3/Level.cs
@@ -4,9 +4,13 @@ namespace SpaceTaxi_3 {
     public class Level {
         
         public string[] map; //an array with the characters representing level tiles
+        
         public string mapName;
-        public List<char> platforms; //amount of platforms in the level
-        public Dictionary<char, string> keyLegend; //legend of which files characters represent
+        
+        public List<char> platforms; //the platforms in the level
+        
+        public Dictionary<char, string> keyLegend; //char legend of which texture files characters represent
+        
         public List<Customer> customers;
 
         public Level(string[] Map, string MapName, List<char> Platforms, Dictionary<char, string> KeyLegend,
