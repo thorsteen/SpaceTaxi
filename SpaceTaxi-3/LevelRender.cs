66bcdda74153a82f3cce417bd6d05f5806b5fc2c
Theodor7 <theodor.b.hansen@gmail.com>
2019-06-04 12:48:06 +0200

    LevelRender refactoring + XML

--- SpaceTaxi-3/LevelRender.cs
+++ SpaceTaxi-3/LevelRender.cs
@@ -6,32 +6,21 @@ using DIKUArcade.Math;
 namespace SpaceTaxi_3 {
     public class LevelRender {
         /// <summary>
-        /// Goes through the array portraying the map that is given. Via two for-loops
-        /// the array goes through each element, and assigns the given path for the picture to the
-        /// char. 
+        /// Takes a Level object and translates its map into entities.
         /// </summary>
-        /// <param name="level"></param>
-        /// <returns>List_Entity </returns>
+        /// <param name="level">A Level object</param>
+        /// <returns>An EntityContainer, containing entities for each wall in the level.</returns>
         public EntityContainer LevelToEntityList(Level level) {
             EntityContainer listEntity = new EntityContainer();
             for (int i = 0; i < 23; i++) {
                 for (int j = 0; j < 40; j++) {
-                    
                     char symbol = level.map[i][j];
                     if (symbol != ' ' && symbol != '^' && symbol != '>') {
                         string fileName = level.keyLegend[symbol];
-                        Entity entity = new Entity(new StationaryShape(new Vec2F(1f / 40f * j, 22f/23f - (1f / 
-                                                                                               23f * i)), new 
-                            Vec2F(
-                                1f / 40f,
-                                1f / 23f)), new Image(Path.Combine("Assets", "Images", fileName)));
-                        listEntity.AddStationaryEntity(new StationaryShape(new Vec2F(1f / 40f * j, 22f/23f - (1f / 
-                                                                                              23f * i)), new 
-                            Vec2F(
-                                1f / 40f,
-                                1f / 23f)),new Image(Path.Combine("Assets", "Images", fileName)));
+                        listEntity.AddStationaryEntity(new StationaryShape(
+                            new Vec2F(1f / 40f * j, 22f/23f - (1f / 23f * i)), 
+                            new Vec2F(1f / 40f,1f / 23f)),new Image(Path.Combine("Assets", "Images", fileName)));
                     }
-                    
                 }
             }
 
