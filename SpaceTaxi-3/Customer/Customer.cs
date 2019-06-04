82cd070b7c8f1602f952da16dc21ff632d9804dd
Theodor7 <theodor.b.hansen@gmail.com>
2019-06-04 12:53:22 +0200

    Customer refactoring + XML

--- SpaceTaxi-3/Customer/Customer.cs
+++ SpaceTaxi-3/Customer/Customer.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.IO;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
@@ -11,21 +10,19 @@ namespace SpaceTaxi_3 {
         private Image customerMoveLeft;
         private Image customerRight;
         private Image customerMoveRight;
-        public List<Customer> CustomerList;
-        public List<Image> CustomerLeftStride;
-        public List<Image> CustomerRightStride;
-        private AnimationContainer walking;
-        private int walkingLenght = 200;
-        public Entity Entity { get; private set; }
 
+        public Entity Entity { get; private set; }
+        
         public string name;
         public int secondsUntilSpawn;
         public char homePlatform;
         public string destinationPlatform;
         public int dropOffTimeLimit;
         public int scoreForDelivery;
+        
         public Vec2I platformCoords;
         public Vec2F myCoords;
+        
         public bool pickedUp;
         public bool delivered;
         
@@ -37,15 +34,14 @@ namespace SpaceTaxi_3 {
             dropOffTimeLimit = DropoffTimeLimit;
             scoreForDelivery = ScoreForDelivery;
 
-            pickedUp = false;
-            delivered = false;
-
             platformCoords = FindSymbolCoords.Find(map, homePlatform);
             myCoords = new Vec2F(1f / 40f * (float)platformCoords.Y,22f/23f - (1f / 23f * (float)platformCoords.X)+1f / 23f);
             
+            pickedUp = false;
+            delivered = false;
+            
             shape = new DynamicShape(myCoords, new Vec2F(0.05f,0.05f));
             Entity = new Entity(shape,new DIKUArcade.Graphics.Image(Path.Combine("Assets","Images","CustomerStandLeft.png")));
-
         }
     }
 }
