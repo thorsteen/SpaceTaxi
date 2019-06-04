11416e1c6f1696c86b617813bb67479c022bd3ab
Theodor7 <theodor.b.hansen@gmail.com>
2019-06-04 12:59:43 +0200

    FindSymbolCoords refactoring + XML

--- SpaceTaxi-3/FindSymbolCoords.cs
+++ SpaceTaxi-3/FindSymbolCoords.cs
@@ -3,7 +3,13 @@ using DIKUArcade.Math;
 
 namespace SpaceTaxi_3 {
     public class FindSymbolCoords {
-
+        
+        /// <summary>
+        /// finds the coordinates of a given char, in an array of strings.
+        /// </summary>
+        /// <param name="map">An array of strings.</param>
+        /// <param name="target">The target char.</param>
+        /// <returns>Vec2I, containing coordinates of the given char.</returns>
         public static Vec2I Find(string[] map, char target) {
             for (int i = 0; i < 23; i++) {
                 for (int j = 0; j < 40 ; j++) {
@@ -13,7 +19,6 @@ namespace SpaceTaxi_3 {
                 }
             }
             throw new Exception("Symbol not in level");
-                
         }
     }
 }
